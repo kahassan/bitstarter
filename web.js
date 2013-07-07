3729e7e3c2eff7f6ad9eb7cf5d72d3b8b50ce29b
John Smith <example@stanford.edu>
2013-07-07 16:19:57 +0000

    updated with index

--- web.js
+++ web.js
@@ -3,8 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ 
+buf = new Buffer(256);
+ var fs = require('fs');
+ fs.readFileSync('/home/ubuntu/bitstarter/index.html')
+ buf.toString(fs);
+
+ response.send(buf);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
